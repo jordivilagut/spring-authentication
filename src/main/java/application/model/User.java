56e92f1966dc11f7d6408287aba30c7498f7fe9f
jordivilagut <vilagutjordi@gmail.com>
2019-07-30 19:41:01 +0200

    Add setters for user

--- src/main/java/application/model/User.java
+++ src/main/java/application/model/User.java
@@ -30,8 +30,16 @@ public class User {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getToken() {
